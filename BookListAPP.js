99b79c5b3229fd3bf74d4dbe13f19979ddca6e33
Tjaytsu <118386185+Tjaytsu@users.noreply.github.com>
2023-07-15 20:11:38 +0100

    BOOKLIST  APPS
    BOOK LIST APP that adds and display books name, book author and its ISBN number.
    Consistently in synchronization with the LOCAL STORAGE used.
    Bootstrap used for the styling purposes.

---
+++ BookListAPP.js
@@ -0,0 +1,14 @@
+// Book Class : Represents a Book
+class Books {
+ constructor(title,author,isbn){
+  this.title  = title;
+  this.author  = author;
+  this.isbn  = isbn;
+ }
+}
+
+
+// UI Class : Handles UI Tasks
+class UI {
+     static 
+}
