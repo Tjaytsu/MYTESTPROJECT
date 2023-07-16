0364745cf0a9055ae72d1226c49e52c63e4b260a
Tjaytsu <118386185+Tjaytsu@users.noreply.github.com>
2023-07-16 08:44:31 +0100

    Update BookListAPP.js
    Creating and Inserting Element for the Alert messages/button into the UI

--- BookListAPP.js
+++ BookListAPP.js
@@ -57,6 +57,21 @@ class UI {
                }
      }
 
+
+     static showAlert(message, className){
+      let div = document.createElement('div');
+      div.className = `alert alert-${className}`;
+      div.appendChild(document.createTextNode(message)); 
+ 
+      let container = document.querySelector('.container');
+      let form = document.querySelector('#book-form');
+
+      container.insertBefore(div,form);
+
+      //Vanish the alert 3second
+      setTimeout(()=> document.querySelector('.alert').remove(),3000);
+     }
+
      static clearFields (){
           document.querySelector('#title').value = '';
           document.querySelector('#author').value = '';
@@ -80,7 +95,12 @@ let form = document.querySelector('#book-form').addEventListener('submit', (e)=>
      let author = document.querySelector('#author').value;
      let isbn = document.querySelector('#isbn').value;
 
-     //instantiate book
+     //Validate fields
+     if(title === '' || author === '' || isbn === ''){
+       UI.showAlert('Please fill in all fields', 'danger');   
+     } else{
+
+          //instantiate book
      let book = new Book(title,author,isbn);
 
      //Add Book to UI
@@ -88,16 +108,27 @@ let form = document.querySelector('#book-form').addEventListener('submit', (e)=>
 
      // console.log(book);
 
+     //show success alert 
+     UI.showAlert('Book Added', 'success');
+
+
      //clear fields for fresh insert
      UI.clearFields ();
+     }
+
 });
 
 
 //Event : Remove a Book
 
-document.querySelector('#book-form').addEventListener('click', (e)=>{
+document.querySelector('#book-list').addEventListener('click', (e)=>{
      // console.log(e.target); 
      UI.deleteBook(e.target);
-})
+
+     //show delete alert 
+      UI.showAlert('Book removed', 'info');
+      
+       
+});
 
 
