f7acef3782f3730d78e826d6b31c52a0b6d2e814
Tjaytsu <118386185+Tjaytsu@users.noreply.github.com>
2023-07-16 06:53:35 +0100

    Book List APP
    VANILLA JAVASCRIPT - NO FRAMEWORKS USED

--- BookListAPP.js
+++ BookListAPP.js
@@ -1,14 +1,83 @@
-// Book Class : Represents a Book
-class Books {
- constructor(title,author,isbn){
-  this.title  = title;
-  this.author  = author;
-  this.isbn  = isbn;
- }
+// Book Class : Represnt a Book
+
+class Book {
+     constructor(title,author,isbn){
+          this.title = title;
+          this.author = author;
+          this.isbn = isbn;
+
+     }
 }
 
-
-// UI Class : Handles UI Tasks
+//UI Class : Handle UI Tasks
 class UI {
-     static 
+     static displayBooks(){
+          //DOMI Local storage for test
+          let StoredBooks = [
+               {
+                    title : 'Book One',
+                    author: 'John Doe',
+                    isbn: '130944'
+               },
+               {
+                    title : 'Book Two',
+                    author: 'John Joseph',
+                    isbn: '150944'
+               },
+               {
+                    title : 'Book Three',
+                    author: 'John Constantine',
+                    isbn: '170944'
+               }
+          ];
+
+          let books = StoredBooks;
+
+          books.forEach((book)=> UI. addBookToList(book));
+     }
+
+     static addBookToList(booky) {
+         let list = document.querySelector('#book-list');
+         let row = document.createElement('tr');
+         row.innerHTML = `
+         <td>${booky.title}</td>
+         <td>${booky.author}</td>
+         <td>${booky.isbn}</td>
+         <td><a href = "#" class = "btn btn-danger btn-sm delete">X</a></td>
+         
+         `; 
+
+         list.appendChild(row);
+     }
 }
+
+//Store Class : Handles Storage
+
+//Event : Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+
+// Event : Add a Book
+let form = document.querySelector('#book-form').addEventListener('submit', (e)=>{
+     //prevent actual/default submit
+     e.preventDefault();
+     //Get form values
+     let title = document.querySelector('#title').value;
+     let author = document.querySelector('#author').value;
+     let isbn = document.querySelector('#isbn').value;
+
+     //instantiate book
+     let book = new Book(title,author,isbn);
+
+     //Add Book to UI
+     UI.addBookToList(book);
+
+     
+     
+     // console.log(book);
+});
+
+
+//Event : Remove a Book
+
+
