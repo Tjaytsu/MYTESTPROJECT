2ae1c5425d4014dc58fe28391daa180ba7379855
Tjaytsu <118386185+Tjaytsu@users.noreply.github.com>
2023-07-20 21:25:54 +0100

    Generator Functions
    Generator Functions LowerCase, Uppercase,Number, and Symbols.
    https://net-comber.com/charset.html

---
+++ PasswordGenerator.js
@@ -0,0 +1,19 @@
+//Generator Functions
+
+function getRandomLower(){
+return String.fromCharCode(Math.floor(Math.random()*26) + 97) ;
+}
+
+function getRandomUpper(){
+return String.fromCharCode(Math.floor(Math.random()*26) + 65) ;
+}
+
+function getRandomNumber(){
+return String.fromCharCode(Math.floor(Math.random()*10) + 48) ;
+}
+
+function getRandomSymbol(){
+let symbols = '!@#$%^&*+(){}[]=<>/,.'
+return symbols[Math.floor(Math.random()* symbols.length)];
+}
+console.log(getRandomSymbol());
