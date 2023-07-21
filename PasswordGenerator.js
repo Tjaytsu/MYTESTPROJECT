aa86df347bdb38a2c1900ffd496069c3739a8039
Tjaytsu <118386185+Tjaytsu@users.noreply.github.com>
2023-07-21 23:16:51 +0100

    Update PasswordGenerator.js

--- PasswordGenerator.js
+++ PasswordGenerator.js
@@ -6,7 +6,7 @@ let lowercaseEl = document.querySelector('#lowercase');
 let numbersEl = document.querySelector('#numbers');
 let symbolsEl = document.querySelector('#symbols');
 let generateEl = document.querySelector('#generate');
-let clickboardEl = document.querySelector('#generate');
+let clipboardEl = document.querySelector('#clipboard');
 
 //Generate Event Listener
 generateEl.addEventListener('click',()=> {
@@ -17,13 +17,69 @@ generateEl.addEventListener('click',()=> {
  let hasSymbol = symbolsEl.checked;
 
   resultEl.innerText = generatePassword(length,hasLower,hasUpper,hasNumber,hasSymbol);
-  
+});
 
-})
+
+//Copy password to clipboard
+
+clipboardEl.addEventListener('click', () =>{
+ let textarea = document.createElement('textarea');
+ let password = resultEl.innerText;
+
+ if(!password){
+     return;
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.focus();
+    textarea.select();
+    navigator.clipboard.writeText(textarea.value);
+    textarea.remove();
+    alert('password copied to clipboard!');
+});
+
+//Generate Password Function
+function generatePassword (length,lower,upper,number,symbol){
+// 1. Init pass Var
+// 2. Filter out unchecked types
+// 3. Loop over length and call generator function for each type
+// 4. Add final pass to pass var and return it which get put into the resultEl.innerText
+
+
+   let generatedPassword = '';
+
+   let typesCount = lower + upper + number + symbol ;
+   // console.log('typesCount: ', typesCount);
+
+   let typeArr = [{lower},{upper},{number},{symbol}].filter(item => Object.values(item)[0]);
+    // console.log('typesArr: ', typeArr);
+
+    if(typesCount === 0){
+     return '';
+    }
+
+    for(let i =0; i<length; i+= typesCount){
+      typeArr.forEach(type =>{
+       let funcName = Object.keys(type)[0];
+
+       // console.log('funcName', funcName);
+
+       generatedPassword += randomFunc[funcName]();
+      })
+    }
+
+    let finalPassword = generatedPassword.slice(0,length) ;
+
+    return finalPassword;
+}
+
+
+
 
 
 //Object with Each functions
-let RandomFunc = {
+let randomFunc = {
  lower : getRandomLower,
  upper : getRandomUpper,
  number : getRandomNumber,
